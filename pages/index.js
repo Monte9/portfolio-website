ca5bcc7e56fef14e178c34861cfba17c8d2e9ee5
Monte Thakkar <manthan.thakkar@gmail.com>
2021-06-23 17:19:26 +0530

    update description in index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,9 @@ export default function Home ({ allProjectsData, allPostsData }) {
           />
         </div>
         <p className="my-4 leading-normal text-2xl">Hello, I'm <b>Monte</b>. I'm a software engineer and an <a href="https://opensea.io/accounts/dearearth" target="_blank">NFT collector</a>.</p>
-        <p className="my-4 leading-normal text-2xl">I currently work as an iOS developer at <a href="https://www.expedia.com/" target="_blank">Expedia</a>. In the past, I've worked as a fullstack engineer @ <a href="https://www.linkedin.com/company/pillow-residential/" target="_blank">Pillow</a> which was <a href="https://techcrunch.com/2018/10/26/expedia-acquires-pillow-and-apartmentjet-to-conquer-the-short-term-rental-market/" target="_blank">acquired by Expedia</a> in 2018.</p>
+        <p className="my-4 leading-normal text-2xl">I currently work as an iOS  developer at <a href="https://www.expedia.com/" target="_blank">Expedia</a>. In the past, I've worked as a fullstack engineer @ <a href="https://www.linkedin.com/company/pillow-residential/" target="_blank">Pillow</a> which was <a href="https://techcrunch.com/2018/10/26/expedia-acquires-pillow-and-apartmentjet-to-conquer-the-short-term-rental-market/" target="_blank">acquired by Expedia</a> in 2018.</p>
+        <p className="my-4 leading-normal text-2xl">I am super bullish on crypto 🚀 especially <a href="https://www.coindesk.com/what-is-defi" target="_blank">Decentralized Finance (DeFi)</a> and my goal is to fully transition to blockchain development by the end of 2022.</p>
+        <p className="my-4 leading-normal text-2xl">Currently available to work on crypto projects as a full-stack developer (including smart contract development in Solidity).</p>
         <p className="my-4 leading-normal text-2xl">You can contact me on <a href="https://twitter.com/MThakkar_" target="_blank">Twitter</a>.</p>
       </section>
       <ProjectsSection projects={allProjectsData} />
